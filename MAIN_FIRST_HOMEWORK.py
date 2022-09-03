0c9e453b30612f92bd35ef6a43a9258f4405bdb1
ILRAYPRO <elvario.vas@gmail.com>
2022-09-03 15:20:49 +0300

    4 Задание вариант без слешов

--- MAIN_FIRST_HOMEWORK.py
+++ MAIN_FIRST_HOMEWORK.py
@@ -5,3 +5,25 @@ Bsecond = 'import __hello__'
 Cthird = 'from __future__ import braces'
 
 print(Afirst, Bsecond, Cthird, sep='\n')
+
+# Представлю два варианта со слэшами и без
+
+'''
+  *       *    *        *        * * *
+  *     * *    * *      *        *   *  
+  *   *   *    *  *     * * *    *  *
+  * *     *    *   *    * * *    *   *  
+'''
+
+# Зададим переменные
+
+i = ' *       *    *        *        * * *'
+l = ' *     * *    * *      *        *   *'
+y = ' *   *   *    *  *     * * *    *  * '
+a = ' * *     *    *   *    * * *    *   *'
+
+print(i, l, y, a, sep='\n')
+
+
+
+
